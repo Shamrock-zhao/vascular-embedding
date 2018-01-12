4e1960509f85a712583238c05e3d13261c9726dc
ignaciorlando <ignaciorlando@gmail.com>
2018-01-12 15:56:02 +0100

    First version of the encoding part

--- learning/models/utils.py
+++ learning/models/utils.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 
 
 
-
 class unetConv2(nn.Module):
+    
     def __init__(self, in_size, out_size, is_batchnorm, dropout):
         super(unetConv2, self).__init__()
 
@@ -34,7 +34,9 @@ class unetConv2(nn.Module):
         return outputs
 
 
+
 class unetUp(nn.Module):
+    
     def __init__(self, in_size, out_size, is_deconv, dropout):
         super(unetUp, self).__init__()
         self.conv = unetConv2(in_size, out_size, False, dropout)
@@ -43,10 +45,52 @@ class unetUp(nn.Module):
         else:
             self.up = nn.Upsample(scale_factor=2)
 
-
     def forward(self, inputs1, inputs2):
         outputs2 = self.up(inputs2)
         offset = outputs2.size()[2] - inputs1.size()[2]
         padding = 2 * [offset // 2, offset // 2]
         outputs1 = F.pad(inputs1, padding)
         return self.conv(torch.cat([outputs1, outputs2], 1))
+
+
+
+class flatten(nn.Module):
+    
+    def __init__(self):
+        super(flatten, self).__init__()
+
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+
+class vascularEmbedding(nn.Module):
+    
+    def __init__(self):
+        super(vascularEmbedding, self).__init__()
+
+        # to flatten the outputs of the convolutional layer
+        self.toReconstruct = flatten()
+        # to map the flattened layer to a linear layer
+        self.encoder1 = nn.Sequential(nn.Linear(16*16*128, 2048), nn.ReLU())
+        # this layer creates the encoding
+        self.fullyEncoded = nn.Sequential(nn.Linear(2048, 128), nn.ReLU())
+        # this restores the 2048 features
+        self.decoder1 = nn.Sequential(nn.Linear(128, 2048), nn.ReLU())
+        # this restores the original output of conv3
+        self.decoderOutput = nn.Sequential(nn.Linear(2048, 16.16.128), nn.ReLU())
+
+    def forward(self, conv3_output):
+        
+        # flatten the input
+        smallFlatConv = self.toReconstruct(conv3_output)
+        # encode it
+        encoded = self.encoder1(smallFlatConv)
+        encoded = self.fullyEncoded(encoded)
+        # decode it
+        decoded = self.decoder1(encoded)
+        decoded = self.decoderOutput(decoded)
+        # reshape
+        return decoded.view(-1, 128, 16, 16)
+
+
+
